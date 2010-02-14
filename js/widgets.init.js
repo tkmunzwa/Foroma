0bee2419d576a3c3ca3a6f08038af8e936dcb64a
(no author) <(no author)@caca0102-0461-431d-9928-c9f2ec7fba4f>
2010-02-14 21:36:21 +0000

    switching to CSS solution for dark datagrid rows
    git-svn-id: http://localhost/svn/repository/bridge/branches/foroma@234 caca0102-0461-431d-9928-c9f2ec7fba4f

--- js/widgets.init.js
+++ js/widgets.init.js
@@ -1,6 +1,12 @@
 /**
- * @author tapiwa
+ * widget.init.js
+ * 
+ * Common behaviour for initializing widgets
+ * 
+ * @author tapiwa@munzwa.tk
+ * 
  */
 $(document).ready(function(){
-	$('table.datagrid tr:odd').css("background-color", "#f5f5ff");
+	$('.autofocus:first').focus(); //put focus on the first element with 'autofocus' class
+	$('table.datagrid tr:odd').addClass("rowdark"); //set color for every odd row
 });
