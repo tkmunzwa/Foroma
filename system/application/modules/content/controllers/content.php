9088efedeab443481b9f7b170febed9a5b902157
(no author) <(no author)@caca0102-0461-431d-9928-c9f2ec7fba4f>
2010-02-14 21:52:31 +0000

    Fixed bug causing warning due to setting of non-existing class variable
    git-svn-id: http://localhost/svn/repository/bridge/branches/foroma@241 caca0102-0461-431d-9928-c9f2ec7fba4f

--- system/application/modules/content/controllers/content.php
+++ system/application/modules/content/controllers/content.php
@@ -6,10 +6,10 @@ class Content extends Controller{
 		parent::Controller();
 		$this->template->set_loader($this->load);
 		if ($msg = $this->session->flashdata('message')){
-			$this->_messages[] = $msg;
+			$this->_info($msg);
 		}
 		if ($msg = $this->session->flashdata('error')){
-			$this->_errors[] = $msg;
+			$this->_error($msg);
 		}
 	}
 	
@@ -23,7 +23,17 @@ class Content extends Controller{
 		//load models into data here.
 		$this->template->write_view('content', 'articles_view', $data);
 		$this->template->render();
-		
+	}
+	
+	function _error($message) {
+		if (!$this->_errors)
+		$this->_errors = array ();
+		$this->_errors[] = $message;
+	}
+	function _info($message) {
+		if (!$this->_infos)
+		$this->_infos = array ();
+		$this->_infos[] = $message;
 	}
 }
 ?>
--- system/application/modules/home/controllers/home.php
+++ system/application/modules/home/controllers/home.php
@@ -2,15 +2,14 @@
 class Home extends Controller{
 	var $_infos = FALSE;
 	var $_errors = FALSE;
+	
 	function Home(){
 		parent::Controller();
 		$this->template->set_loader($this->load);
-		if ($msg = $this->session->flashdata('message')){
-			$this->_messages[] = $msg;
-		}
-		if ($msg = $this->session->flashdata('error')){
-			$this->_errors[] = $msg;
-		}
+		if ($message = $this->session->flashdata('message'))
+			$this->_info($message);	
+		if ($e = $this->session->flashdata('error'))
+			$this->_error($e);	
 	}
 	
 	function index(){
@@ -19,6 +18,17 @@ class Home extends Controller{
 		//load models into data here.
 		$this->template->write_view('content', 'home_view', $data);
 		$this->template->render();
+	}
+	
+	function _error($message) {
+		if (!$this->_errors)
+		$this->_errors = array ();
+		$this->_errors[] = $message;
+	}
+	function _info($message) {
+		if (!$this->_infos)
+		$this->_infos = array ();
+		$this->_infos[] = $message;
 	}
 }
 ?>
